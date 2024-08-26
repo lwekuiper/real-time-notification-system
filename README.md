be19ed9809bfed8325813093af8f521993842217
Laurens Kuiper <l@urens.me>
2024-08-26 14:22:51 +0200

    Authorize front end to listen to private channels

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - [x] Install Laravel Sanctum
 - [x] Create notification trigger through API route
 - [x] Seed database with 2 users
-- [ ] Authorize front end to listen to private channels
+- [x] Authorize front end to listen to private channels
 - [ ] Create Vue component NotificationComponent displaying list of notifications with message and timestamp
 - [ ] Create Vue component NotificationCount displaying a badge with number of unread (?) notifications
 - [ ] Make notifications readable?
--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,7 +5,23 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    encrypted: true,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    authorizer: (channel, options) => {
+        return {
+            authorize: (socketId, callback) => {
+                axios.post('/api/broadcasting/auth', {
+                    socket_id: socketId,
+                    channel_name: channel.name
+                })
+                .then(response => {
+                    callback(false, response.data);
+                })
+                .catch(error => {
+                    callback(true, error);
+                });
+            }
+        };
+    }
 });
