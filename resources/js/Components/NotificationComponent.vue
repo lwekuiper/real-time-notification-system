58a6eeda62086755d773bc0a1eed61138dd43305
Laurens Kuiper <l@urens.me>
2024-08-26 16:38:27 +0200

    Fix typo on NotificationComponent

--- resources/js/Components/NotificationComponent.vue
+++ resources/js/Components/NotificationComponent.vue
@@ -40,7 +40,7 @@ onUnmounted(() => {
 <template>
     <section class="space-y-6">
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Notifcations</h2>
+            <h2 class="text-lg font-medium text-gray-900">Notifications</h2>
 
             <p class="mt-1 text-sm text-gray-600">
                 Here are your notifications.
